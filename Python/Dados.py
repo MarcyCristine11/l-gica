0a9a33e9d6159a2d9acff9a470a864378652f555
MarcyCristine11 <50590848+MarcyCristine11@users.noreply.github.com>
2019-05-13 14:03:41 -0300

    Mudança 3

--- Python/Dados.py
+++ Python/Dados.py
@@ -1,17 +1,23 @@
-#Meu primeiro programa!
+# Meu primeiro programa!
 
-from random import randint #Importa a função de gerar números aleatórios<3
+from random import randint  # Importa a função de gerar números aleatórios<3
 
 print("Jodo de Dados")
 
-dado1 = randint(1, 6) #Gera um número entre 1 e 6 aleatoriamente<3
-print("Dado 1: ",dado1) 
+dado1 = randint(1, 6)  # Gera um número entre 1 e 6 aleatoriamente<3
+print("Dado 1: ", dado1)
 
 dado2 = randint(1, 6)
-print("Dado 2: ",dado2)
+print("Dado 2: ", dado2)
 
 dado3 = randint(1, 6)
-print("Dado 3: ",dado3)
+print("Dado 3: ", dado3)
 
 dado4 = randint(1, 6)
-print("Dado 4: ",dado4) 
+print("Dado 4: ", dado4)
+
+jogador1 = dado1 + dado2
+jogador2 = dado3 + dado4
+
+print('Jogador 1: ', jogador1)
+print('Jogador 2: ', jogador2)
